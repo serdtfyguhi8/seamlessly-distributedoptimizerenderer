a668a9c17f4406fce9d1c17c03d3ec844b504f11
serdtfyguhi8 <js1agagafan@hotmail.com>
2024-05-05 13:48:38 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-function isValidSudoku(board) {
-  for (let i = 0; i < 9; i++) {
-    const row = new Set();
-    const col = new Set();
-    const box = new Set();
-    for (let j = 0; j < 9; j++) {
-      const rowVal = board[i][j];
-      const colVal = board[j][i];
-      const boxVal =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-      if (rowVal !== ".") {
-        if (row.has(rowVal)) return false;
-        row.add(rowVal);
-      }
-      if (colVal !== ".") {
-        if (col.has(colVal)) return false;
-        col.add(colVal);
-      }
-      if (boxVal !== ".") {
-        if (box.has(boxVal)) return false;
-        box.add(boxVal);
-      }
+function minMeetingRooms(intervals) {
+  if (intervals.length === 0) return 0;
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  minHeap.insert(intervals[0][1]);
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] >= minHeap.peek()) minHeap.pop();
+    minHeap.insert(intervals[i][1]);
+  }
+  return minHeap.size();
+}
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+  insert(val) {
+    this.heap.push(val);
+    this.bubbleUp();
+  }
+  pop() {
+    const min = this.heap[0];
+    const last = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.bubbleDown();
+    }
+    return min;
+  }
+  peek() {
+    return this.heap[0];
+  }
+  size() {
+    return this.heap.length;
+  }
+  bubbleUp() {
+    let current = this.heap.length - 1;
+    while (current > 0) {
+      const parent = Math.floor((current - 1) / 2);
+      if (this.heap[current] >= this.heap[parent]) break;
+      [this.heap[current], this.heap[parent]] = [
+        this.heap[parent],
+        this.heap[current],
+      ];
+      current = parent;
     }
   }
-  return true;
+  bubbleDown() {
+    let current = 0;
+    while (true) {
+      let left = current * 2 + 1;
+      let right = current * 2 + 2;
+      let smallest = left;
+      if (right < this.heap.length && this.heap[right] < this.heap[left])
+        smallest = right;
+      if (left >= this.heap.length || this.heap[current] <= this.heap[smallest])
+        break;
+      [this.heap[current], this.heap[smallest]] = [
+        this.heap[smallest],
+        this.heap[current],
+      ];
+      current = smallest;
+    }
+  }
 }
